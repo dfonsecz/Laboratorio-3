d3982f60068248ba2d62db397c610f17b7c5bd63
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-05-07 19:43:39 -0600

    Modified ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Funcion factorial
 
 int factorial (int n) {
     int i = 1;
@@ -9,10 +12,27 @@ int factorial (int n) {
     return i;
 }
 
+// Funcion principal que recibe argumentos en la linea de comandos
+
 int main (int argc, char *argv[]) {
-    int fac4 = factorial(4);
-    int fac5 = factorial(5);
-    printf("4! = %d, 5! = %d\n", fac4, fac5);
+
+    // Verificar si se proporcionan 2 argumentos
+    if (argc != 3) { // Se considera que debe ser 3 porque en argc[0] se guarda el nombre del programa
+        printf("No se proporcionaron suficientes argumentos\n");
+        return 1; // Indica que el programa no se ejecuta adecuadamente y ocurre un error
+    }
+
+    // Convertir character string a int
+    int n1 = atoi(argv[1]);
+    int n2 = atoi(argv[2]);
+
+    // Evaluar funcion factorial() para los argumentos ingresados
+    int facn1 = factorial(n1);
+    int facn2 = factorial(n2);
+
+    // Imprimir mensaje con los resultados de los calculos
+    printf("%d! = %d, %d! = %d\n", n1, facn1, n1, facn2);
+
     return 0;
 }
 
