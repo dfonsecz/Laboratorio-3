1422c1dead334ee4bed475cc1961fd778f943c27
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-05-07 18:59:42 -0600

    Modified ejercicio2.c

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+int factorial (int n) {
+    int i = 1;
+    while (n > 1) {
+        i = i * n;
+        n = n - 1; // Corregido - se quito la declaracion de la variable, era redundante
+    }
+    return i;
+}
+
+int main (int argc, char *argv[]) {
+    int fac4 = factorial(4);
+    int fac5 = factorial(5);
+    printf("4! = %d, 5! = %d\n", fac4, fac5);
+    return 0;
+}
+
